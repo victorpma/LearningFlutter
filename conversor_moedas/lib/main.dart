fff82c923d456f1f65ceaa6bd8e0731c27784df8
Victor Paixão <vpma_amorim@hotmail.com>
2019-06-16 22:28:08 -0300

    Implementando interface e as configurações iniciais

--- conversor_moedas/lib/main.dart
+++ conversor_moedas/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const request = "https://api.hgbrasil.com/finance?format=json&key=5091ebd6";
 
 void main() {
   runApp(MaterialApp(
     title: "Conversor de Moedas",
+    color: Colors.amber,
+    theme: ThemeData(hintColor: Colors.amber, primaryColor: Colors.white),
     home: HomePage(),
   ));
 }
@@ -13,8 +19,75 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Future<Map> _obterCotacao() async {
+    http.Response response = await http.get(request);
+
+    if (response != null) return json.decode(response.body);
+
+    throw new Exception();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+            title: Text("Conversor de Moedas"),
+            backgroundColor: Colors.amber,
+            centerTitle: true),
+        body: FutureBuilder<Map>(
+          future: _obterCotacao(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: CircularProgressIndicator(
+                    backgroundColor: Colors.amber,
+                  ),
+                );
+              default:
+                return SingleChildScrollView(
+                  padding: EdgeInsets.all(10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      Icon(Icons.monetization_on,
+                          color: Colors.amber, size: 150.0),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            labelText: "Reais",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefix: Text("R\$")),
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      ),
+                      Divider(),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            labelText: "Dólares",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefix: Text("US\$")),
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      ),
+                      Divider(),
+                      TextField(
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                            labelText: "Euros",
+                            labelStyle: TextStyle(color: Colors.amber),
+                            border: OutlineInputBorder(),
+                            prefix: Text("€")),
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      )
+                    ],
+                  ),
+                );
+            }
+          },
+        ));
   }
 }
