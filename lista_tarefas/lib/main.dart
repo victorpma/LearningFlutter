17354019a82a7f8612d75f93757985f88c152270
Victor Paixão <vpma_amorim@hotmail.com>
2019-06-20 22:16:06 -0300

    Implementando métodos de leitura e escrita de dados

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -13,6 +17,32 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List tarefas = [];
+
+  Future<File> _obterArquivo() async {
+    final diretorio = await getApplicationDocumentsDirectory();
+
+    return File("${diretorio.path}/tarefas.json");
+  }
+
+  Future<File> _salvarDados() async {
+    var dados = json.encode(tarefas);
+
+    final arquivo = await _obterArquivo();
+
+    return arquivo.writeAsString(dados);
+  }
+
+  Future<String> _lerDados() async {
+    try {
+      final arquivo = await _obterArquivo();
+
+      return arquivo.readAsString();
+    } catch (ex) {
+      return null;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
