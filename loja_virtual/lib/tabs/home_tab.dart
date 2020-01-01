e5a75e91f3c503b5801045c417f576df676c2637
Victor <vpma_amorim@hotmail.com>
2020-01-01 20:57:34 -0300

    Add SliverStaggeredGrid of images in home_tab

--- loja_virtual/lib/tabs/home_tab.dart
+++ loja_virtual/lib/tabs/home_tab.dart
@@ -1,5 +1,8 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomeTab extends StatelessWidget {
   @override
@@ -16,23 +19,54 @@ class HomeTab extends StatelessWidget {
     Widget _buildSliverAppBar() => CustomScrollView(
           slivers: <Widget>[
             SliverAppBar(
-              floating: true,
-              snap: true,
-              backgroundColor: Colors.transparent,
-              elevation: 0.0,
-              flexibleSpace: FlexibleSpaceBar(
-                title: const Text("Novidades"),
-                centerTitle: true,
-              ),
+                floating: true,
+                snap: true,
+                backgroundColor: Colors.transparent,
+                elevation: 0.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  title: const Text("Novidades"),
+                  centerTitle: true,
+                )),
+            FutureBuilder<QuerySnapshot>(
+              future: Firestore.instance
+                  .collection("images_novidades")
+                  .orderBy("position")
+                  .getDocuments(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return SliverToBoxAdapter(
+                    child: Container(
+                      height: 200.0,
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                      ),
+                    ),
+                  );
+                } else {
+                  return SliverStaggeredGrid.count(
+                      mainAxisSpacing: 1.0,
+                      crossAxisSpacing: 1.0,
+                      crossAxisCount: 2,
+                      staggeredTiles: snapshot.data.documents.map((document) {
+                        return StaggeredTile.count(
+                            document.data["width"], document.data["height"]);
+                      }).toList(),
+                      children: snapshot.data.documents.map((document) {
+                        return FadeInImage.memoryNetwork(
+                          placeholder: kTransparentImage,
+                          image: document.data["image"],
+                          fit: BoxFit.cover,
+                        );
+                      }).toList());
+                }
+              },
             )
           ],
         );
 
     return Stack(
-      children: <Widget>[
-        _buildBodyBack(), 
-        _buildSliverAppBar()
-        ],
+      children: <Widget>[_buildBodyBack(), _buildSliverAppBar()],
     );
   }
 }
