9890383093d65aa834150387efff9bb3659678c8
Victor <vpma_amorim@hotmail.com>
2020-01-01 21:37:13 -0300

    Add CustomDrawer in home_page

--- loja_virtual/lib/pages/home_page.dart
+++ loja_virtual/lib/pages/home_page.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/widgets/custom_drawer.dart';
 
 class HomePage extends StatelessWidget {
   final _pageController = PageController();
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
-      controller: _pageController,
-      physics: NeverScrollableScrollPhysics(),
-      children: <Widget>[
-        HomeTab(),
-      ],
-    );
+        return Scaffold(
+        drawer: CustomDrawer(),
+        body: PageView(
+          controller: _pageController,
+          physics: NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            HomeTab(),
+          ],
+        ));
   }
 }
